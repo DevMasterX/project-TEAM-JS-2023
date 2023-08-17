50533cb376e246435e2f98c52383d7331cde555d
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-08-17 21:32:17 +0300

    update

--- src/js/filter.js
+++ src/js/filter.js
@@ -6,29 +6,31 @@ import { forEach } from 'lodash';
 const filterForm = document.querySelector(".filter-form-input");
 const selectes = document.querySelectorAll(".filter-form-select")
 
-const selectTime = document.getElementById('searchTime')
+const selectTime = document.getElementById("searchTime")
 console.dir(selectTime);
-
-function createSelectTimeOptions() {
-    const minTime = 5;
-    const maxTime = 120;
-    const step = 5;
-    const options = [];
-
-    for (let time = minTime; time <= maxTime; time += step) {
-        options.push(` <option class="filter-form-select-time " value="">${time} min</option>`)
-    }
-
-    const optionMarkup = options.join('');
-    selectTime.insertAdjacentHTML('beforeend', optionMarkup)
-
-}
-createSelectTimeOptions()
-filterForm.addEventListener('submit', handlerFilterForm)
+filterForm.addEventListener('submit', handlerFilterForm);
 
 selectes.forEach(item => {
+    const options = [];
+    let maxShowItems = 6; // За замовчуванням
+    if (item === selectTime) {
+        const minTime = 5;
+        const maxTime = 120;
+        const step = 5;
+        maxShowItems = 6;
+
+        for (let time = minTime; time <= maxTime; time += step) {
+            options.push({ text: `${time} min`, value: time.toString() });
+        }
+    } else {
+        // тут будуть умови для інших селектів 
+    }
+
     new SlimSelect({
-        select: item
+        select: item,
+        data: options,
+        openPosition: 'down',
+        maxShowItems: maxShowItems
     });
 });
 
