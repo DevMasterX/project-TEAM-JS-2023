8c774fa7f1c428ada8ee3ae3396c10488b6ae652
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2023-08-19 16:46:50 +0000

    Deploying to gh-pages from @ DevMasterX/project-TEAM-JS-2023@dbd8d457df19aa21049cdfe0df2583c006d43386 🚀

---
+++ favorites.5748bc31.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".filter-gallery-item-btn"),c=document.querySelector(".r-modal-backdrop");e.addEventListener("click",function(e){c.classList.remove("visually-hidden"),console.log("click")}),c.addEventListener("click",function(e){c.classList.add("visually-hidden"),console.log("click")});
+//# sourceMappingURL=favorites.5748bc31.js.map
