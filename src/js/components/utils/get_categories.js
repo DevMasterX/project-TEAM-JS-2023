614758930a64fe433db2ce9ca52a5fb2752c5461
DevMasterX <sokol080288@gmail.com>
2023-08-21 02:13:20 +0300

    123

--- src/js/components/utils/get_categories.js
+++
@@ -1,4 +0,0 @@
-export function getCategories(data) {
-    const categories = [...data].flatMap(item => item.categories)
-   return [...new Set(categories)].map(item => item?.toLowerCase())
-}
--- src/js/components/utils/index.js
+++
@@ -1,3 +0,0 @@
-export * from './dom'
-export * from './get_categories'
-export * from './handle'
