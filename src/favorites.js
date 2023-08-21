84a612084d30cca5234f426bc334eb6487ae1471
Lyuda313 <Lyuda313@gmail.com>
2023-08-21 20:29:52 +0300

    favorites_v3_fix

--- src/favorites.js
+++ src/favorites.js
@@ -3,6 +3,7 @@ import './js/components';
 import './js/api.js';
 import './js/btn-to-top';
 import './js/modal_window_recipe.js';
-import './js/order.js';
 import './js/pagination.js';
 import './js/theme.js';
+
+// import './js/modal_our_team.js';
