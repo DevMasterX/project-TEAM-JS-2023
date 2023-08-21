c40a32de0a58cfe71d61473fec0f6172d30cd811
DevMasterX <sokol080288@gmail.com>
2023-08-22 01:36:22 +0300

    comment local storage, filter js

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,104 +1,101 @@
-const form = document.querySelector('.modal-form');
-const listForStorage = document.querySelector('.filter-gallery-list');
+// const form = document.querySelector('.modal-form');
+// const listForStorage = document.querySelector('.filter-gallery-list');
 
-listForStorage.addEventListener('click', e => {
-  const cardBody = e.target.parentElement.parentElement;
-  const recipeInfo = {
-    id: cardBody.id,
-    name: cardBody.querySelector('.filter-gallery-item-tittle').textContent,
-    image: cardBody.querySelector('.filter-gallery-item-photo').src,
-    rating: cardBody.querySelector('.filter-gallery-item-rating-value')
-      .textContent,
-    description: cardBody.querySelector('.filter-gallery-item-description')
-      .textContent,
-  };
+// listForStorage.addEventListener('click', e => {
+//   const cardBody = e.target.parentElement.parentElement;
+//   const recipeInfo = {
+//     id: cardBody.id,
+//     name: cardBody.querySelector('.filter-gallery-item-tittle').textContent,
+//     image: cardBody.querySelector('.filter-gallery-item-photo').src,
+//     rating: cardBody.querySelector('.filter-gallery-item-rating-value')
+//       .textContent,
+//     description: cardBody.querySelector('.filter-gallery-item-description')
+//       .textContent,
+//   };
 
-  // const favFav = document.querySelector('.filter-gallery-item-favorit-btn');
-  // favFav?.addEventListener('click', ()=> alert('fff'))
-export function onFavBtnClick () {console.log('gi')}
+//   // const favFav = document.querySelector('.filter-gallery-item-favorit-btn');
+//   // favFav?.addEventListener('click', ()=> alert('fff'))
+// export function onFavBtnClick () {console.log('gi')}
 
+// listForStorage.addEventListener('click', (e)=>{
+// const favFav = e.target.classList;
+// if(favFav.value === 'filter-gallery-item-favorit-btn'){
+// console.dir(favFav)
+//   const favArr=document.getElementsByClassName('filter-gallery-item-favorit-btn');
+//    console.log(favArr)}
+//     const cardBody = e.target.parentElement.parentElement;
 
+//     const recipeInfo = {
+//         category: cardBody.dataset.attribute,
+//         id: cardBody.id,
+//         name: cardBody.querySelector('.filter-gallery-item-tittle').textContent,
+//         image:cardBody.querySelector('.filter-gallery-item-photo').src,
+//         rating: cardBody.querySelector('.filter-gallery-item-rating-value').textContent,
+//         description: cardBody.querySelector('.filter-gallery-item-description').textContent,
+//     }
 
+//     if(cardBody.classList.contains('favorite') && e.target.nodeName === 'H3'){
 
-listForStorage.addEventListener('click', (e)=>{
-const favFav = e.target.classList;
-if(favFav.value === 'filter-gallery-item-favorit-btn'){
-console.dir(favFav)
-  const favArr=document.getElementsByClassName('filter-gallery-item-favorit-btn');
-   console.log(favArr)}
-    const cardBody = e.target.parentElement.parentElement;
+//         recipeDB.removeFromDB(recipeInfo.name)
+//         cardBody.classList.remove('favorite')
 
-    const recipeInfo = {
-        category: cardBody.dataset.attribute,
-        id: cardBody.id,
-        name: cardBody.querySelector('.filter-gallery-item-tittle').textContent,
-        image:cardBody.querySelector('.filter-gallery-item-photo').src,
-        rating: cardBody.querySelector('.filter-gallery-item-rating-value').textContent,
-        description: cardBody.querySelector('.filter-gallery-item-description').textContent,
-    }
- 
-    if(cardBody.classList.contains('favorite') && e.target.nodeName === 'H3'){  
-  
-        recipeDB.removeFromDB(recipeInfo.name)
-        cardBody.classList.remove('favorite')
-      
-    }
- 
-        if(cardBody.classList.contains('favorite'))return;
-   
-    if(e.target.nodeName === 'BUTTON'){
+//     }
 
-  if (cardBody.classList.contains('favorite')) return;
+//         if(cardBody.classList.contains('favorite'))return;
 
-  if (e.target.nodeName === 'P') {
-    cardBody.classList.add('favorite');
+//     if(e.target.nodeName === 'BUTTON'){
 
-    recipeDB.saveIntoDB(recipeInfo);
-  }
-});
+//   if (cardBody.classList.contains('favorite')) return;
 
-class RecipeDB {
-  getOrderFromLC() {
-    let orderForm;
-    if (!localStorage.getItem('orderForm')) {
-      orderForm = {};
-    } else {
-      orderForm = JSON.parse(localStorage.getItem('orderForm'));
-    }
-    return orderForm;
-  }
-  setOrderFormLC() {
-    const infoOrder = {
-      name: form.elements.name.value,
-      phone: form.elements.phone.value,
-      email: form.elements.email.value,
-    };
-    localStorage.setItem('orderForm', JSON.stringify(infoOrder));
-  }
-  saveIntoDB(recipe) {
-    const recipes = this.getFromDB();
-    recipes.push(recipe);
-    localStorage.setItem('recipes', JSON.stringify(recipes));
-  }
-  getFromDB() {
-    let recipes;
-    if (!localStorage.getItem('recipes')) {
-      recipes = [];
-    } else {
-      recipes = JSON.parse(localStorage.getItem('recipes'));
-    }
-    return recipes;
-  }
-  removeFromDB(name) {
-    const recipes = this.getFromDB();
-    recipes.forEach((recipe, index) => {
-      if (name === recipe.name) {
-        recipes.splice(index, 1);
-      }
-    });
-    localStorage.setItem('recipes', JSON.stringify(recipes));
-  }
-}
-const recipeDB = new RecipeDB();
+//   if (e.target.nodeName === 'P') {
+//     cardBody.classList.add('favorite');
 
-form.addEventListener('input', () => recipeDB.setOrderFormLC());
+//     recipeDB.saveIntoDB(recipeInfo);
+//   }
+// });
+
+// class RecipeDB {
+//   getOrderFromLC() {
+//     let orderForm;
+//     if (!localStorage.getItem('orderForm')) {
+//       orderForm = {};
+//     } else {
+//       orderForm = JSON.parse(localStorage.getItem('orderForm'));
+//     }
+//     return orderForm;
+//   }
+//   setOrderFormLC() {
+//     const infoOrder = {
+//       name: form.elements.name.value,
+//       phone: form.elements.phone.value,
+//       email: form.elements.email.value,
+//     };
+//     localStorage.setItem('orderForm', JSON.stringify(infoOrder));
+//   }
+//   saveIntoDB(recipe) {
+//     const recipes = this.getFromDB();
+//     recipes.push(recipe);
+//     localStorage.setItem('recipes', JSON.stringify(recipes));
+//   }
+//   getFromDB() {
+//     let recipes;
+//     if (!localStorage.getItem('recipes')) {
+//       recipes = [];
+//     } else {
+//       recipes = JSON.parse(localStorage.getItem('recipes'));
+//     }
+//     return recipes;
+//   }
+//   removeFromDB(name) {
+//     const recipes = this.getFromDB();
+//     recipes.forEach((recipe, index) => {
+//       if (name === recipe.name) {
+//         recipes.splice(index, 1);
+//       }
+//     });
+//     localStorage.setItem('recipes', JSON.stringify(recipes));
+//   }
+// }
+// const recipeDB = new RecipeDB();
+
+// form.addEventListener('input', () => recipeDB.setOrderFormLC());
