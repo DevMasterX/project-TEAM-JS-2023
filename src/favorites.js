d9e76c1b0fa568064537886e56779a916dae8a9a
Lyuda313 <Lyuda313@gmail.com>
2023-08-21 19:43:44 +0300

    1

--- src/favorites.js
+++
@@ -1,9 +0,0 @@
-import './js/components';
-
-import './js/api.js';
-import './js/btn-to-top';
-import './js/modal_window_recipe.js';
-import './js/pagination.js';
-import './js/theme.js';
-
-// import './js/modal_our_team.js'
--- src/js/components/markup/category_markup.js
+++
@@ -1,8 +0,0 @@
-export function markupCategory(text, currentCategory) {
-    const html = `
-        <span class="my-favorites-category__item-list text-item-category">
-            ${text}
-        </span>
-    `
-    return html
-}
