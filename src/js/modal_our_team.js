1e3a9a2b9ddb8ea37e6612370d0e41eade909545
DevMasterX <sokol080288@gmail.com>
2023-08-22 12:03:04 +0300

    mod team

--- src/js/modal_our_team.js
+++ src/js/modal_our_team.js
@@ -1,43 +1,43 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const modal = document.querySelector('[data-modal-team]');
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.querySelector('[data-modal-team]');
 
-//   const openBtn = document.querySelector('.btnOpenNow');
+  const openBtn = document.querySelector('.btnOpenNow');
 
-//   const closeBtn = document.querySelector('[data-team-close]');
+  const closeBtn = document.querySelector('[data-team-close]');
 
-//   // Функція відкриття модального вікна
-//   function openModal() {
-//     modal.classList.remove('is-hidden');
+  // Функція відкриття модального вікна
+  function openModal() {
+    modal.classList.remove('is-hidden');
 
-//     document.body.classList.add('no-scroll');
-//   }
+    document.body.classList.add('no-scroll');
+  }
 
-//   // Функція закриття модального вікна
-//   function closeModal() {
-//     modal.classList.add('is-hidden');
+  // Функція закриття модального вікна
+  function closeModal() {
+    modal.classList.add('is-hidden');
 
-//     document.body.classList.remove('no-scroll');
-//   }
+    document.body.classList.remove('no-scroll');
+  }
 
-//   // Закриття на Escape
-//   function closeOnEscape(e) {
-//     if (e.key === 'Escape' && !modal.classList.contains('is-hidden')) {
-//       closeModal();
-//     }
-//   }
+  // Закриття на Escape
+  function closeOnEscape(e) {
+    if (e.key === 'Escape' && !modal.classList.contains('is-hidden')) {
+      closeModal();
+    }
+  }
 
-//   // Закриття на клік за межами модалки
-//   function closeOnOverlayClick(e) {
-//     if (e.target === modal) {
-//       closeModal();
-//     }
-//   }
+  // Закриття на клік за межами модалки
+  function closeOnOverlayClick(e) {
+    if (e.target === modal) {
+      closeModal();
+    }
+  }
 
-//   openBtn.addEventListener('click', openModal);
+  openBtn.addEventListener('click', openModal);
 
-//   closeBtn.addEventListener('click', closeModal);
+  closeBtn.addEventListener('click', closeModal);
 
-//   document.addEventListener('keydown', closeOnEscape);
+  document.addEventListener('keydown', closeOnEscape);
 
-//   modal.addEventListener('click', closeOnOverlayClick);
-// });
+  modal.addEventListener('click', closeOnOverlayClick);
+});
