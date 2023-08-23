c84e1e6e5df0d49fc5d15b8131b61d359cf5b6be
Oleksandr Voloshun <voloshun3337@gmail.com>
2023-08-23 08:01:31 +0200

    team-music

--- src/js/btn-to-top.js
+++ src/js/btn-to-top.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   initScrollButton();
-  initFeedbackForm();
+  // playMusic();
+  // initFeedbackForm();
 });
 
-const SCROLL_THRESHOLD = 3;
+const SCROLL_THRESHOLD = 300;
 const EMAIL_REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
 function initScrollButton() {
@@ -16,57 +17,49 @@ function initScrollButton() {
       button.classList.remove('is-visible');
     }
   }
-
   if (button) {
     button.addEventListener('click', () => {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     });
-
     window.addEventListener('scroll', checkButtonVisibility);
   }
 }
 
-function initFeedbackForm() {
-  const { feedbackForm, emailInput } = getFeedbackElements();
+// let play = document.getElementById('teamOpenBtn');
+// function playMusic() {
+//   let audio = new Audio('audio.mp3');
+//   audio.play();
+// }
+// play.addEventListener('click', playMusic);
 
-  // Завантаження збереженої електронної адреси з localStorage
-  const savedEmail = localStorage.getItem('userEmail');
-  if (savedEmail && emailInput) {
-    emailInput.value = savedEmail;
-  }
-
-  if (feedbackForm && emailInput) {
-    feedbackForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-
-      if (!isValidEmail(emailInput.value)) {
-        alert('Please enter a valid email address.');
-        return; // Зупинка обробки, якщо email не є дійсним
-      }
-
-      // Зберігаємо електронну адресу в localStorage
-      localStorage.setItem('userEmail', emailInput.value);
-
-      // Очищаємо поле введення
-      emailInput.value = '';
-
-      // Показуємо спливаюче вікно з підтвердженням
-      alert('Thank you for sending your email address!');
-    });
-  }
-}
-
-function getFeedbackElements() {
-  const feedbackForm =
-    document.getElementById('feedbackForm') ||
-    document.querySelector('.feedback-section form');
-  const emailInput = feedbackForm
-    ? feedbackForm.querySelector('input[type="email"]')
-    : null;
-
-  return { feedbackForm, emailInput };
-}
-
-function isValidEmail(email) {
-  return EMAIL_REGEX.test(email);
-}
+// function initFeedbackForm() {
+//   const { feedbackForm, emailInput } = getFeedbackElements();
+//   const savedEmail = localStorage.getItem('userEmail');
+//   if (savedEmail && emailInput) {
+//     emailInput.value = savedEmail;
+//   }
+//   if (feedbackForm && emailInput) {
+//     feedbackForm.addEventListener('submit', function (event) {
+//       event.preventDefault();
+//       if (!isValidEmail(emailInput.value)) {
+//         alert('Please enter a valid email address.');
+//         return;
+//       }
+//         localStorage.setItem('userEmail', emailInput.value);
+//           emailInput.value = '';
+//         alert('Thank you for sending your email address!');
+//     });
+//   }
+// }
+// function getFeedbackElements() {
+//   const feedbackForm =
+//     document.getElementById('feedbackForm') ||
+//     document.querySelector('.feedback-section form');
+//   const emailInput = feedbackForm
+//     ? feedbackForm.querySelector('input[type="email"]')
+//     : null;
+//   return { feedbackForm, emailInput };
+// }
+// function isValidEmail(email) {
+//   return EMAIL_REGEX.test(email);
+// }
