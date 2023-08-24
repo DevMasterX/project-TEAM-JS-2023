7be534bb684af6e5d0f3991e40537880d3b8d5f5
KostiantynSS <storoghenko94@gmail.com>
2023-08-24 19:30:31 +0300

    theme changes

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,11 +1,20 @@
 
 const themeToggle = document.getElementById("theme-toggle");
+const themeToggleDark = document.getElementById('theme-toggle-dark')
 const currentTheme = localStorage.getItem("theme");
 if (!currentTheme){localStorage.setItem('theme', 'light-theme')}
 else {
     document.body.classList.add(currentTheme);
 }
-if (currentTheme === 'dark-theme'){themeToggle.checked = true;}
+if (currentTheme === 'dark-theme'){themeToggle.checked = true; themeToggleDark.checked = true}
+themeToggleDark.addEventListener('click', ()=>{ if (document.body.classList.contains("dark-theme")) {
+        
+    document.body.classList.replace("dark-theme", "light-theme");
+    localStorage.setItem("theme", "light-theme");
+} else { 
+    document.body.classList.replace("light-theme", "dark-theme");
+    localStorage.setItem("theme", "dark-theme");
+}})
 themeToggle.addEventListener("click", () => {
    
 
