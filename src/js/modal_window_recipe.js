9fada0fd5fd0115382986f51e01fcf2c73abbe52
Lissa692 <125963481+Lissa692@users.noreply.github.com>
2023-08-25 11:24:09 +0300

    new

--- src/js/modal_window_recipe.js
+++ src/js/modal_window_recipe.js
@@ -2,7 +2,7 @@ import CSS from '../css/styles.css';
 import { getRecipeDetails } from './api';
 import { renderIcons } from './rating';
 
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 const modalWindow = document.querySelector('.r-modal-content');
 const seeModal = document.querySelector('.r-modal-backdrop');
 const ratingBtn = document.querySelector('.r-modal-rating-btn');
@@ -12,35 +12,48 @@ const favoriteBtn = document.querySelector('.r-modal-favorite-btn');
 let toId = '';
 
 function eventOpenrModal() {
-    const getIdElelment = document.querySelector('.filter-gallery-list');
-    getIdElelment.addEventListener('click', openModal);
+  const getIdElelment = document.querySelector('.filter-gallery-list');
+  getIdElelment.addEventListener('click', openModal);
 }
 
 function eventOpenrModalTwo() {
-    const getIdElelmentTwo = document.querySelector('.popular-js');
-    getIdElelmentTwo.addEventListener('click', openModal);
+  const getIdElelmentTwo = document.querySelector('.popular-js');
+  getIdElelmentTwo.addEventListener('click', openModal);
 }
 
 function loadContent() {
-    return getRecipeDetails(toId).then( data => modalWindow.insertAdjacentHTML('afterbegin', addContent(data))).catch(err => console.log(err));
+  return getRecipeDetails(toId)
+    .then(data =>
+      modalWindow.insertAdjacentHTML('afterbegin', addContent(data))
+    )
+    .catch(err => console.log(err));
 }
 
 // console.log(getRecipeDetails(toId).then( data => console.log(data)));
 
 function addContent(arr) {
-    const {title, instructions, ingredients, video, preview, rating, tags, time } = arr;
+  const {
+    title,
+    instructions,
+    ingredients,
+    video,
+    preview,
+    rating,
+    tags,
+    time,
+  } = arr;
 
-    let newTags ='';
-    tags.forEach(element => {
-        newTags+= `<span class="r-modal-tag">#${element}</span>`;
-    });
+  let newTags = '';
+  tags.forEach(element => {
+    newTags += `<span class="r-modal-tag">#${element}</span>`;
+  });
 
-    let newIngredients ='';
-    ingredients.forEach(element => {
-        newIngredients+= `<div class="r-modal-ingerdients-name">${element.name}<div class="r-modal-ingerdients-value">${element.measure}</div></div>`
-    });
+  let newIngredients = '';
+  ingredients.forEach(element => {
+    newIngredients += `<div class="r-modal-ingerdients-name">${element.name}<div class="r-modal-ingerdients-value">${element.measure}</div></div>`;
+  });
 
-    return `
+  return `
     <video class="r-modal-video"
     src="${video}"
     poster="${preview}"
@@ -51,7 +64,9 @@ function addContent(arr) {
     ></video>
     <h2 class="r-modal-name">${title.toUpperCase()}</h2>
     <div class="r-modal-info-container">
-    <div class="r-modal-rating-container"><div class="r-modal-rating">${rating.toFixed(1)}</div>
+    <div class="r-modal-rating-container"><div class="r-modal-rating">${rating.toFixed(
+      1
+    )}</div>
     <div class="r-modal-star-wrap">
         <svg class="r-modal-rating-icon" width="18" height="18">
             <use href="../images/star.svg#icon-star"></use>
@@ -73,55 +88,49 @@ function addContent(arr) {
     <div class="r-modal-ingerdients-container">${newIngredients}</div>
     <div class="r-modal-tags">${newTags}</div>
     <p class="r-modal-instructions">${instructions}</p>`;
-};
+}
 
-function openModal (event) {
-    if(!event.target.id) {
-        return;
-    }
+function openModal(event) {
+  if (!event.target.id) {
+    return;
+  }
 
-    toId = event.target.id;
-    loadContent();
-    seeModal.classList.remove('visually-hidden');
-    body.classList.add("no-scroll");
-    document.addEventListener('keydown', closeOnEscape);
-    seeModal.addEventListener('click', closeOnTarget);
-    ratingBtn.addEventListener('click', closeOnTarget);
-    closeBtn.addEventListener('click', closeOnTarget);
-    favoriteBtn.addEventListener('click', closeOnTarget);
-
-
+  toId = event.target.id;
+  loadContent();
+  seeModal.classList.remove('visually-hidden');
+  body.classList.add('no-scroll');
+  document.addEventListener('keydown', closeOnEscape);
+  seeModal.addEventListener('click', closeOnTarget);
+  ratingBtn.addEventListener('click', closeOnTarget);
+  closeBtn.addEventListener('click', closeOnTarget);
+  favoriteBtn.addEventListener('click', closeOnTarget);
 }
 
 function closeModal(event) {
-    seeModal.classList.add('visually-hidden');
-    body.classList.remove("no-scroll");
+  seeModal.classList.add('visually-hidden');
+  body.classList.remove('no-scroll');
 
-    modalWindow.innerHTML = '';
+  modalWindow.innerHTML = '';
 
-    document.removeEventListener('keydown', closeOnEscape);
-    seeModal.removeEventListener('click', closeModal);
-    ratingBtn.removeEventListener('click', closeOnTarget);
-    closeBtn.removeEventListener('click', closeOnTarget);
-    favoriteBtn.removeEventListener('click', closeOnTarget);
+  document.removeEventListener('keydown', closeOnEscape);
+  seeModal.removeEventListener('click', closeModal);
+  ratingBtn.removeEventListener('click', closeOnTarget);
+  closeBtn.removeEventListener('click', closeOnTarget);
+  favoriteBtn.removeEventListener('click', closeOnTarget);
 }
 
 function closeOnTarget(event) {
-    if(event.target === event.currentTarget) {
-        closeModal();
-    }
+  if (event.target === event.currentTarget) {
+    closeModal();
+  }
 }
 
 function closeOnEscape(e) {
-    if (e.key === 'Escape') {
-      closeModal();
-    }
+  if (e.key === 'Escape') {
+    closeModal();
   }
+}
 
-export {
-    eventOpenrModal,
-    eventOpenrModalTwo,
-    toId
-};
+export { eventOpenrModal, eventOpenrModalTwo, toId };
 
 // {/* <div class="r-modal-rating-stars-icon">&#9734; &#9734; &#9734; &#9734; &#9734;</div> */}
