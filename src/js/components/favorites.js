51dc7d937d4c532e96b217d32f1602f640a52389
Lyuda313 <Lyuda313@gmail.com>
2023-08-25 11:14:44 +0300

    favorites fix

--- src/js/components/favorites.js
+++ src/js/components/favorites.js
@@ -37,7 +37,6 @@ if (!localstorageFavorites) {
     initialState.favorites = localstorageFavorites
 }
 
-
 // window.addEventListener("resize", () => {
 //     const w  = window.matchMedia("(max-width: 768px)")
 //     if (w?.matches) {
